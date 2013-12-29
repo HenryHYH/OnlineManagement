723571baa5c9fcc6450e0c726689aefdb5181fac
HenryHYH <henry.hyh@gmail.com>
2013-12-29 19:54:48 +0800

    Toggle aside

---
+++ Website/App/Directive/ToggleOffCanvasDirective.js
@@ -0,0 +1,11 @@
+﻿omApp.directive('toggleOffCanvas', function () {
+    return {
+        restrict: "A",
+        link: function link(scope, element, attrs) {
+            element.click(function () {
+                $("#" + attrs.toggleOffCanvas).toggleClass("active");
+                element.children("i").toggleClass("fa-chevron-right");
+            });
+        }
+    };
+});
