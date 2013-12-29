5831c21de920ebb516a69a8d9f1370303a153fce
HenryHYH <henry.hyh@gmail.com>
2013-12-29 19:58:22 +0800

    Toggle aside button can custom icon

--- Website/App/Directive/ToggleOffCanvasDirective.js
+++ Website/App/Directive/ToggleOffCanvasDirective.js
@@ -2,7 +2,7 @@
     return {
         restrict: "A",
         link: function link(scope, element, attrs) {
-            element.click(function () {
+            element.append('<i class="fa fa-chevron-left"></i>').click(function () {
                 $("#" + attrs.toggleOffCanvas).toggleClass("active");
                 element.children("i").toggleClass("fa-chevron-right");
             });
